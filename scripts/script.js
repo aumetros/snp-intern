20bc7bcb817a959f57ee94b456e34dc380848dc7
aumetros <aum3tros@yandex.ru>
2023-01-22 23:10:35 +0300

    Сделать стиль дропдауна через js

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+const formSelect = document.querySelector('.collect-tour__form-select');
+
+function changeColorSelect() {
+  formSelect.style = 'color: #1B1F2B';
+}
+
+formSelect.addEventListener('click', changeColorSelect);
